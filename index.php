e0007b8a1b184ac0efb5c78dd393cdd56567e46b
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-09-10 08:59:03 +0700

    Update code PHP 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>Hello Tâm đù</div>
+</body>
+</html>
+
+<?php 
+$soa = 6;
+$sob = 11;
+$tong = $soa + $sob;
+$hieu = $sob - $soa;
+
+echo $tong . "<br>" . $hieu;
+?>
